d1d6be607bbefb4546f61c4d9779819fe5baed49
Þorgils Jonsson <toggi@trackwell.com>
2026-01-04 00:57:38 +0000

    csrf exempt on register

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 from .serializers import UserSerializer, RegisterSerializer
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
