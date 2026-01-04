70b20d316a2d91f39cea67f6f8e20a14e45d832d
Þorgils Jonsson <toggi@trackwell.com>
2026-01-04 00:26:59 +0000

    update settings for cors.

--- backend/peyjabankinn/settings.py
+++ backend/peyjabankinn/settings.py
@@ -125,7 +125,11 @@ SIMPLE_JWT = {
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
 else:
-    CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+    CORS_ALLOWED_ORIGINS = [
+        "https://peyjabanki.com",
+        "http://localhost:3000",
+    ]
+
 
 # --------------------
 # HTTPS / proxy headers
